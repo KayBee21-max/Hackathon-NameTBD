83bc9182171bb903d534a6fc0b699ab28ef08bca
Struggler178 <2609046@students.wits.ac.za>
2025-09-04 08:59:29 +0200

    changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// serve static files (optional)
+
 app.use(express.static(path.resolve('./')));
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
